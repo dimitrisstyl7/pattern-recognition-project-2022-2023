a7645dcb4ba94957bea4f38a0b6b636d239bf9f2
dimitrisstyl7 <dimitris.volley1911@gmail.com>
2023-02-04 18:25:47 +0200

    HOLA

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import pandas as pd
 from sklearn.preprocessing import MinMaxScaler, OneHotEncoder
 import numpy as np
+import scipy.stats as stats
 import math
 
 
@@ -14,16 +15,9 @@ def one_hot_encoding(data):
     return OneHotEncoder().fit_transform(data.reshape(-1, 1)).toarray()
 
 def normalize_data(data):
-    scaler = MinMaxScaler()
-    return scaler.fit_transform(data)
-    
-if __name__ == '__main__':
-    numerical_data, categorical_data = load_data()
-    numerical_columns_names = numerical_data.columns
-    categorical_data = list(categorical_data)
-    categorical_data_encoded = one_hot_encoding(np.array(categorical_data))
-    counter = 0
-    
+    return MinMaxScaler((0,1)).fit_transform(data.reshape(-1, 1))
+
+def check_values(numerical_data):
     for label in numerical_data:
         column = sorted(list(numerical_data[label]))
         if len(column) % 2 == 0:
@@ -33,4 +27,25 @@ if __name__ == '__main__':
         for i in range(len(numerical_data[label])):
             if math.isnan(numerical_data[label][i]):
                     numerical_data[label][i] = median
-                    counter += 1
+    return numerical_data
+
+def calculate_pdf(data):
+    pdf_list = []
+    for i in range(len(data)):
+        column = list(data[i])
+        pdf = stats.norm.pdf(column, np.mean(column), np.std(column))
+        pdf_list.append(pdf)
+    return pdf_list
+
+if __name__ == '__main__':
+    numerical_data, categorical_data = load_data()
+    categorical_data_encoded = one_hot_encoding(np.array(list(categorical_data)))
+    numerical_data = check_values(numerical_data)
+    normalized_numerical_data = normalize_data(np.array(numerical_data))
+    pdf_list = calculate_pdf(normalized_numerical_data)
+    
+    #data = list(categorical_data_encoded)
+    #pdf_list.append(stats.norm.pdf(data, np.mean(data), np.std(data)))
+    data = list(categorical_data_encoded)
+    pdf_list.append(stats.norm.pdf(data, np.mean(data), np.std(data)))
+    print(len(pdf_list))
